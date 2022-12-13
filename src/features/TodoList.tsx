fcccb0987ef4743f4b234c2420007f148a71db76
Rashadul Haque <mohammed.haque@iths.se>
2022-12-13 12:59:44 +0100

    Added some styles

--- src/features/TodoList.tsx
+++ src/features/TodoList.tsx
@@ -30,79 +30,103 @@ const TodoList = () => {
         justifyContent: "center",
         alignItems: "center",
         height: "100vh",
+        border:'solid 4px #ff781f',
+        color:'#fff'
       }}
     >
-      <form onSubmit={addNewTodo} style={{ width: "fit-content", padding: 20 }}>
-        <input
-          placeholder="Skriv en todo..."
-          onChange={(event) => setNewTodoText(event.target.value)}
-          value={newTodoText}
-        ></input>
-        <button type="submit" disabled={newTodoText.length === 0}>
-          Lägg till
-        </button>
-      </form>
-      <h2
+      <div
         style={{
-          textAlign: "center",
-          border: "solid .5px #000",
-          padding: "2px 10px",
-          display: "flex",
-          justifyContent: "center",
-          alignItems: "center",
+          padding: "20px",
+          boxShadow: "0px 10px 15px -3px rgba(0,0,0,0.1)",
+          background: "#ff781f",
+          minHeight: "500px",
+          borderRadius: "8px",
         }}
       >
-        Todo
-        <button
-          onClick={sortTodo}
-          style={{
-            background: "#000",
-            color: "#fff",
-            borderRadius: "2px",
-            marginLeft: "12px",
-          }}
-          disabled={todos.length < 2}
+        <form
+          onSubmit={addNewTodo}
+          style={{ width: "fit-content", padding: 20 }}
         >
-          {filterDescending ? (
-            <FaArrowUp style={{ fontSize: ".7rem" }} />
-          ) : (
-            <FaArrowDown style={{ fontSize: ".7rem" }} />
-          )}
-        </button>
-      </h2>
-
-      {todos.length ? (
-        <ul
+          <input
+            placeholder="Skriv en todo..."
+            onChange={(event) => setNewTodoText(event.target.value)}
+            value={newTodoText}
+          ></input>
+          <button type="submit" disabled={newTodoText.length === 0}>
+            Lägg till
+          </button>
+        </form>
+        <h2
           style={{
-            width: "200px",
-            padding: "12px 24px",
+            textAlign: "center",
             border: "solid .5px #000",
+            padding: "2px 10px",
+            display: "flex",
+            justifyContent: "center",
+            alignItems: "center",
           }}
         >
-          {todos.map((todo) => (
-            <li key={todo.id} style={{ paddingLeft: "4px",textTransform:'capitalize' }}>
-              {todo.text}{" "}
-              <button
-                onClick={() => dispatch(removeTodo(todo.id))}
+          Todo
+          <button
+            onClick={sortTodo}
+            style={{
+              background: "#000",
+              color: "#fff",
+              borderRadius: "2px",
+              marginLeft: "12px",
+            }}
+            disabled={todos.length < 2}
+          >
+            {filterDescending ? (
+              <FaArrowUp style={{ fontSize: ".7rem" }} />
+            ) : (
+              <FaArrowDown style={{ fontSize: ".7rem" }} />
+            )}
+          </button>
+        </h2>
+
+        {todos.length ? (
+          <ul
+            style={{
+              width: "200px",
+              padding: "12px 24px",
+            }}
+          >
+            {todos.map((todo) => (
+              <li
+                key={todo.id}
                 style={{
-                  width: "fit-content",
-                  background: "#000",
-                  color: "#fff",
-                  padding: "2px",
-                  borderRadius: "4px",
-                  marginLeft: "12px",
+                  paddingLeft: "4px",
+                  textTransform: "capitalize",
+                  fontWeight: "500",
+                  textAlign:'center'
+                 
                 }}
               >
-                Ta bort
-              </button>
-            </li>
-          ))}
-        </ul>
-      ) : (
-        <p>Du har inga todos</p>
-      )}
+                {todo.text}{" "}
+                <button
+                  onClick={() => dispatch(removeTodo(todo.id))}
+                  style={{
+                    width: "fit-content",
+                    background: "#000",
+                    color: "#fff",
+                    padding: "2px",
+                    borderRadius: "4px",
+                    marginLeft: "12px",
+                  }}
+                >
+                  Ta bort
+                </button>
+              </li>
+            ))}
+          </ul>
+        ) : (
+          <p style={{textAlign:'center'}}>Du har inga todos</p>
+        )}
+      </div>
     </div>
   );
 };
 
 export default TodoList;
+//
